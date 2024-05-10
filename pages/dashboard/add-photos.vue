3292561b9a5519b0fc4a63e66befad9d41ffbc4c
Yudi Alviska <yudi.a@weefer.co.id>
2024-05-10 16:43:11 +0700

    edit page add-photos

--- pages/dashboard/add-photos.vue
+++ pages/dashboard/add-photos.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
 import { format } from "date-fns";
 
+import { object, string, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+
+definePageMeta({
+  layout: false,
+});
+
+const schema = object({
+  email: string().email("Invalid email").required("Required"),
+  password: string()
+    .min(6, "Must be at least 6 characters")
+    .required("Required"),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+  price: undefined,
+  location: undefined,
+  date: undefined,
+  description: undefined,
+  tag: [],
+});
+const loading = ref(false);
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // loading.value = true;
+  // const config = useRuntimeConfig();
+  // const response = await $fetch(`${config.public.baseUrl}/posts`, {
+  //   method: "POST",
+  //   body: event.data,
+  // });
+  // console.log(response);
+  // loading.value = false;
+  // navigateTo("/dashboard");
+}
+
 definePageMeta({
   middleware: "auth",
 });
@@ -13,19 +50,50 @@ function onChangeDate(value: Date) {
 </script>
 
 <template>
-  <UPopover :popper="{ placement: 'bottom-start' }">
-    <UButton
-      icon="i-heroicons-calendar-days-20-solid"
-      :label="format(date, 'd MMM, yyy')"
-    />
+  <div class="flex flex-1 p-4">
+    <UCard class="flex flex-1 p-4">
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormGroup label="Price" name="price">
+          <UInput v-model="state.price" />
+        </UFormGroup>
 
-    <template #panel="{ close }">
-      <DatePicker
-        :model-value="null"
-        is-required
-        @close="close"
-        @update:model-value="onChangeDate"
-      />
-    </template>
-  </UPopover>
+        <UFormGroup label="Location" name="location">
+          <UInput v-model="state.location" type="location" />
+        </UFormGroup>
+
+        <template #panel="{ close }">
+          <DatePicker
+            :model-value="null"
+            is-required
+            @close="close"
+            @update:model-value="onChangeDate"
+          />
+        </template>
+
+        <UButton type="submit" :loading="loading" :disabled="loading">
+          Save Change
+        </UButton>
+      </UForm>
+      <!-- <UPopover :popper="{ placement: 'bottom-start' }">
+        <UButton
+          icon="i-heroicons-calendar-days-20-solid"
+          :label="format(date, 'd MMM, yyy')"
+        />
+
+        <template #panel="{ close }">
+          <DatePicker
+            :model-value="null"
+            is-required
+            @close="close"
+            @update:model-value="onChangeDate"
+          />
+        </template>
+      </UPopover> -->
+    </UCard>
+  </div>
 </template>
