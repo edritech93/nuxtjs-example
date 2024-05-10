1689b456fa4284c5e1b86f5c6056dde5981f1a80
Yudi Alviska <yudi.a@weefer.co.id>
2024-05-10 15:25:55 +0700

    add v-calendar

---
+++ pages/dashboard/add-photos.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { format } from "date-fns";
+
+definePageMeta({
+  middleware: "auth",
+});
+
+const date = ref(new Date());
+
+function onChangeDate() {}
+</script>
+
+<template>
+  <UPopover :popper="{ placement: 'bottom-start' }">
+    <UButton
+      icon="i-heroicons-calendar-days-20-solid"
+      :label="format(date, 'd MMM, yyy')"
+    />
+
+    <template #panel="{ close }">
+      <DatePicker :model-value="null" is-required @close="close" />
+    </template>
+  </UPopover>
+</template>
