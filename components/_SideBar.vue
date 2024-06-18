a0019f635555e1c5a23d7f41818cd447f4ee1fcc
Yudi Alviska <yudi.a@weefer.co.id>
2024-06-18 15:01:08 +0700

    rename file Sidebar

--- components/_SideBar.vue
+++ components/_SideBar.vue
@@ -2,10 +2,10 @@
   <div class="flex flex-col min-h-screen p-2 border-r border-gray-100/30">
     <div class="flex flex-col flex-1">
       <NuxtLink to="/" class="mb-4">
-        <ItemSideBar icon="icon/upload-white-24.png" name="Dashboard" />
+        <ItemSideBar icon="svg/svg-upload-24.svg" name="Dashboard" />
       </NuxtLink>
       <NuxtLink to="/profile">
-        <ItemSideBar icon="icon/active-white-24.png" name="Profile" />
+        <ItemSideBar icon="svg/svg-upload-24.svg" name="Profile" />
       </NuxtLink>
     </div>
   </div>
