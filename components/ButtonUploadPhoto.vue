2db568c05792266c490dad6918b68f771db89905
Yudi Alviska <yudi.a@weefer.co.id>
2024-06-20 11:25:49 +0700

    edit ButtonUploadPhoto, support multiple photo

--- components/ButtonUploadPhoto.vue
+++ components/ButtonUploadPhoto.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+const file = ref<FileList>();
 const onChangeFile = (event: Event) => {
-  console.log(event);
+  const _file = (event.target as HTMLInputElement).files as FileList;
+  file.value = _file;
+  console.log(_file);
 };
 </script>
 
 <template>
   <label class="bg-primary px-4 py-1 rounded-md text-sm">
     Upload Photo
-    <input type="file" style="display: none" required @change="onChangeFile" />
+    <input
+      type="file"
+      style="display: none"
+      required
+      multiple
+      @change="onChangeFile"
+    />
   </label>
 </template>
