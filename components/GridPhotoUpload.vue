2f5691f67c952f79e318357478ff553f760e8b71
Yudi Alviska <yudi.a@weefer.co.id>
2024-06-20 16:42:54 +0700

    edit GridPhotoUpload

--- components/GridPhotoUpload.vue
+++ components/GridPhotoUpload.vue
@@ -2,29 +2,29 @@
 const { dataPhoto } = defineProps<{
   dataPhoto: FileList;
 }>();
-function imagePreviewFunc() {
-  const imagePreview_2 = document.getElementById("imagePreview_2");
+function loadPhoto() {
+  const divImage = document.getElementById("div_image");
   for (let i = 0; i < dataPhoto.length; i++) {
-    let imager = document.createElement("img");
-    imager.src = URL.createObjectURL(dataPhoto[i]);
-    if (imagePreview_2) {
-      imagePreview_2.append(imager);
+    let image = document.createElement("img");
+    image.src = URL.createObjectURL(dataPhoto[i]);
+    image.className =
+      "max-w-[150px] border-yellow-400 border-2 bg-gray-400 rounded";
+    image.id = i.toString();
+    if (divImage) {
+      divImage.append(image);
     }
   }
 }
 onMounted(() => {
-  imagePreviewFunc();
+  loadPhoto();
 });
 </script>
 
 <template>
   <div
     class="flex-1 gap-4 m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 grid-rows-6"
-  >
-    <div v-for="photo in dataPhoto">
-      <div class="max-w-[150px] border-yellow-400 border-2 bg-gray-400 rounded">
-        <div id="imagePreview_2"></div>
-      </div>
-    </div>
-  </div>
+    id="div_image"
+  ></div>
+
+  <img />
 </template>
